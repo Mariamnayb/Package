97de5f66a88e632c68de7bbdc70128cd34c9ab83
Mariam <mariamhakobyan1991@gmail.com>
2019-07-16 04:57:29 +0400

    Initial commit

---
+++ src/Circle.java
@@ -0,0 +1,22 @@
+class Circle
+{
+    public Circle ();
+
+    private String color;
+    private double redius;
+
+
+    public static void main(String[] args) {
+
+    }
+    Circle = new Circle()
+
+    {
+
+        System.out.println(Circle);
+
+
+    }
+
+    }
+
